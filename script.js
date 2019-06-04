a04ee850a4a07dd261887e52842440276caada8a
BloodAngel58 <bloodange2013@yandex.ru>
2019-06-04 14:20:39 +0300

    More information to output

--- script.js
+++ script.js
@@ -30,26 +30,74 @@ function searchData(url) {
     .catch(error => console.log(error));
 }
 
-function addItem(img_url, summary) {
+function addItem(
+  img_url,
+  summary,
+  construction_year,
+  room_number,
+  bedroom_number,
+  price_formatted,
+  title,
+  keywords
+) {
   const divImg = document.createElement("img");
-  const divText = document.createElement("div");
-  //Родитель
-  const todoItem = document.createElement("div");
+  const divSummary = document.createElement("div");
+  const divConstruction_year = document.createElement("div");
+  const divRoom_number = document.createElement("div");
+  const divBedroom_number = document.createElement("div");
+  const divPrice_formatted = document.createElement("div");
+  const textTitle = document.createElement("h3");
+  const textKeywords = document.createElement("h4");
 
+  //
+  const divContent = document.createElement("div"); // div для инфы
+  const todoItem = document.createElement("div"); //Родитель
+  //
   divImg.src = img_url;
-  divText.innerHTML = summary;
-
-  divText.classList.add("loaded-text__item");
+  divSummary.innerHTML = summary;
+  divConstruction_year.innerHTML = "Дата постройки :" + construction_year;
+  divRoom_number.innerHTML = "Количество комнат :" + room_number;
+  divBedroom_number.innerHTML = "Количество спален :" + bedroom_number;
+  divPrice_formatted.innerHTML = "Цена :" + price_formatted;
+  textTitle.innerHTML = title;
+  textKeywords.innerHTML = keywords;
+  //
+  divContent.classList.add("loaded-text__content");
   divImg.classList.add("loaded-img__item");
   todoItem.classList.add("loaded-itemList__search");
-
+  textTitle.classList.add("loaded-text-content__item");
+  textKeywords.classList.add("loaded-text-content__item");
+  divConstruction_year.classList.add("loaded-text-content__item");
+  divRoom_number.classList.add("loaded-text-content__item");
+  divBedroom_number.classList.add("loaded-text-content__item");
+  divPrice_formatted.classList.add("loaded-text-content__item");
+  divSummary.classList.add("loaded-text-content__item");
+  //
   todoItem.appendChild(divImg);
-  todoItem.appendChild(divText);
+  divContent.appendChild(textTitle);
+  divContent.appendChild(textKeywords);
+  divContent.appendChild(divSummary);
+  divContent.appendChild(divConstruction_year);
+  divContent.appendChild(divRoom_number);
+  divContent.appendChild(divBedroom_number);
+  divContent.appendChild(divPrice_formatted);
+
+  //родители
+  todoItem.appendChild(divContent);
   listItem.appendChild(todoItem);
 }
 function loadItem(itemList) {
   for (let key = 0; key < itemList.length; key++) {
-    addItem(itemList[key].img_url, itemList[key].summary);
+    addItem(
+      itemList[key].img_url,
+      itemList[key].summary,
+      itemList[key].construction_year,
+      itemList[key].room_number || "Информация отсутствует",
+      itemList[key].bedroom_number || "Информация отсутствует",
+      itemList[key].price_formatted,
+      itemList[key].title,
+      itemList[key].keywords
+    );
   }
 }
 /*
@@ -58,5 +106,6 @@ img_url;
 //keywords;
 price_formatted;
 room_number;
+bedroom_number
 summary;
 */
