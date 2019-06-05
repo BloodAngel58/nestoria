d46365b7fb7f963360f01f4b2c7b2fbe02f9184c
BloodAngel58 <bloodange2013@yandex.ru>
2019-06-05 16:11:44 +0300

    add modalWindow and favorits

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let arr = [];
+let arrFavorit = [];
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const url =
-  "https://api.nestoria.de/api?encoding=json&pretty=1&action=search_listings&country=de&listing_type=buy&place_name=";
-
+  "https://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=uk&listing_type=buy&place_name=";
+document.getElementById("listItem").addEventListener("click", updatCheck, true);
 document.getElementById("buttonSearch").addEventListener("click", loadingData);
+document.getElementById("myModal").addEventListener("click", deletetCheck);
 const inputTextSearch = document.getElementById("inputText");
 const listItem = document.getElementById("listItem");
+const modalItem = document.getElementById("myModal");
+const modalWindow = document.getElementById("md-w");
+const favorits = document.getElementById("favorits");
 function loadingData() {
   let textSearch = inputTextSearch.value;
   if (inputTextSearch.value) {
@@ -30,23 +35,69 @@ function searchData(url) {
     .catch(error => console.log(error));
 }
 
-function addItem(
-  key,
-  img_url,
-  summary,
-  construction_year,
-  room_number,
-  bedroom_number,
-  price_formatted,
-  title,
-  keywords
-) {
+function updatCheck(event) {
+  const target = event.target;
+  if (target.type == "submit") {
+    let key = target.parentNode.getAttribute("key");
+    console.log(key);
+    addModalItem(arr[key], key);
+  }
+}
+function deletetCheck(event) {
+  const target = event.target;
+  console.log(target.type);
+  if (target.type == "reset") {
+    modalWindow.style.display = "none";
+    target.parentNode.innerHTML = "";
+  }
+  if (target.type == "submit") {
+    let key = target.parentNode.getAttribute("key");
+    addToFavorit(key);
+  }
+}
+function addToFavorit(key) {
+  const obj = arr[key];
+  if (!arrFavorit.includes(obj)) {
+    arrFavorit.push(obj);
+    favorits.innerHTML = "";
+    loadFavorit();
+  }
+}
+function loadFavorit() {
+  for (let key = 0; key < arrFavorit.length; key++) {
+    drawingFavorit(
+      key,
+      arrFavorit[key].img_url,
+      arrFavorit[key].price_formatted,
+      arrFavorit[key].title
+    );
+  }
+}
+function drawingFavorit(key, img_url, title, price_formatted) {
+  const divImg = document.createElement("img");
+  const textTitle = document.createElement("h3");
+  const divPrice_formatted = document.createElement("div");
+  //
+  const divContent = document.createElement("div"); // div для инфы
+  const todoItem = document.createElement("div"); //Родитель
+  //
+  divImg.src = img_url;
+  textTitle.innerHTML = title;
+  divPrice_formatted.innerHTML = "Цена :" + price_formatted;
+  ///
+  todoItem.setAttribute("key", key);
+  ///
+  divContent.appendChild(textTitle);
+  divContent.appendChild(divPrice_formatted);
+  todoItem.appendChild(divImg);
+  todoItem.appendChild(divContent);
+  favorits.appendChild(todoItem);
+}
+
+function addItem(key, img_url, summary, price_formatted, title, keywords) {
   const MoreDetailedButton = document.createElement("button");
   const divImg = document.createElement("img");
   const divSummary = document.createElement("div");
-  const divConstruction_year = document.createElement("div");
-  const divRoom_number = document.createElement("div");
-  const divBedroom_number = document.createElement("div");
   const divPrice_formatted = document.createElement("div");
   const textTitle = document.createElement("h3");
   const textKeywords = document.createElement("h4");
@@ -56,13 +107,10 @@ function addItem(
   //
   divImg.src = img_url;
   divSummary.innerHTML = summary;
-  divConstruction_year.innerHTML = "Дата постройки :" + construction_year;
-  divRoom_number.innerHTML = "Количество комнат :" + room_number;
-  divBedroom_number.innerHTML = "Количество спален :" + bedroom_number;
   divPrice_formatted.innerHTML = "Цена :" + price_formatted;
   textTitle.innerHTML = title;
   textKeywords.innerHTML = keywords;
-  MoreDetailedButton.innerHTML = "More Detaile";
+  MoreDetailedButton.innerHTML = "More Detailes";
   //
   MoreDetailedButton.classList.add("more-detaile__button");
   divContent.classList.add("loaded-text__content");
@@ -70,9 +118,6 @@ function addItem(
   todoItem.classList.add("loaded-itemList__search");
   textTitle.classList.add("loaded-text-content__item");
   textKeywords.classList.add("loaded-text-content__item");
-  divConstruction_year.classList.add("loaded-text-content__item");
-  divRoom_number.classList.add("loaded-text-content__item");
-  divBedroom_number.classList.add("loaded-text-content__item");
   divPrice_formatted.classList.add("loaded-text-content__item");
   divSummary.classList.add("loaded-text-content__item");
   //
@@ -82,24 +127,19 @@ function addItem(
   divContent.appendChild(textTitle);
   divContent.appendChild(textKeywords);
   divContent.appendChild(divSummary);
-  divContent.appendChild(divConstruction_year);
-  divContent.appendChild(divRoom_number);
-  divContent.appendChild(divBedroom_number);
   divContent.appendChild(divPrice_formatted);
   //родители
   todoItem.appendChild(divContent);
   todoItem.appendChild(MoreDetailedButton);
   listItem.appendChild(todoItem);
 }
+
 function loadItem(itemList) {
   for (let key = 0; key < itemList.length; key++) {
     addItem(
       key,
       itemList[key].img_url,
       itemList[key].summary,
-      itemList[key].construction_year,
-      itemList[key].room_number || "Информация отсутствует",
-      itemList[key].bedroom_number || "Информация отсутствует",
       itemList[key].price_formatted,
       itemList[key].title,
       itemList[key].keywords
@@ -107,23 +147,50 @@ function loadItem(itemList) {
   }
 }
 
-function updatCheck(event) {
-  const target = event.target;
-  if (target.type == "checkbox") {
-    if (target.checked) {
-      target.parentNode.classList.add("todo-item__checked");
-    } else {
-      target.parentNode.classList.remove("todo-item__checked");
-    }
-  }
-  let key = target.parentNode.getAttribute("key");
-
-  if (target.type == "submit") {
-    deleteTasks(event.target.parentNode, key);
-  }
-}
-
-function deleteTasks(node, key) {
-  node.remove();
-  todoList.splice(todoList.findIndex(item => item.idTask === key), 1);
+function addModalItem(item, key) {
+  modalWindow.style.display = "flex";
+  const divContent = document.createElement("div"); // div для инфы
+  //
+  const exitButton = document.createElement("button");
+  const addToFavoritesButton = document.createElement("button");
+  const modalImg = document.createElement("img");
+  const modalSummary = document.createElement("div");
+  const modalPrice_formatted = document.createElement("div");
+  const modalTextTitle = document.createElement("h3");
+  const modalTextKeywords = document.createElement("h4");
+  const modalLister_name = document.createElement("h5");
+  const modalBedroom_number = document.createElement("div");
+  const modalProperty_type = document.createElement("div");
+  //
+  exitButton.type = "reset";
+  exitButton.innerHTML = "&#10006";
+  addToFavoritesButton.innerHTML = "add to favorites";
+  modalImg.src = item.img_url;
+  modalSummary.innerHTML = item.summary;
+  modalPrice_formatted.innerHTML = item.price_formatted;
+  modalTextTitle.innerHTML = item.title;
+  modalTextKeywords.innerHTML = item.keywords;
+  //
+  modalLister_name.innerHTML = "lister_name :" + item.lister_name;
+  modalBedroom_number.innerHTML = "bedroom_number :" + item.bedroom_number;
+  modalProperty_type.innerHTML = "Property_type :" + item.property_type;
+  ////
+  divContent.classList.add("modal-content");
+  modalImg.classList.add("modal-img__item");
+  addToFavoritesButton.classList.add("favorit-button");
+  ////
+  divContent.setAttribute("key", key);
+  //
+  divContent.appendChild(modalTextTitle);
+  divContent.appendChild(modalLister_name);
+  divContent.appendChild(modalTextKeywords);
+  divContent.appendChild(modalSummary);
+  divContent.appendChild(modalBedroom_number);
+  divContent.appendChild(modalProperty_type);
+  divContent.appendChild(modalPrice_formatted);
+  divContent.appendChild(addToFavoritesButton);
+  //
+  modalItem.appendChild(modalImg);
+  modalItem.appendChild(divContent);
+  modalItem.appendChild(exitButton);
 }
